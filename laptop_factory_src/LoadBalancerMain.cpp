229a17f5cd9561c6548010a939674f1831489d33
babatunde <bbabatunde@users.noreply.github.com>
2021-11-15 14:47:36 -0500

    Update LoadBalancerMain.cpp

--- laptop_factory_src/LoadBalancerMain.cpp
+++ laptop_factory_src/LoadBalancerMain.cpp
@@ -5,6 +5,38 @@
 
 int main(int argc, char *argv[]) {
 
-    std::cout<<"hello"<<std::endl;
+    int port = atoi(argv(1));
+    int number_of_servers = atoi(argv[2]);
+    int cache_size = atoi(argv[3]);
+
+
+    LoadBalancerSocket socket;
+    LoadBalancerStub balancerStub;
+    std::vector<PeerInfo> primaryServers; //@TODO create new struct for primary servers info
+
+    int offset = 4;
+
+    for(int i = offset; i < number_of_servers; i++){
+
+        int peer_unique_id = atoi(argv[offset++]);
+        std::string peer_ip = argv[offset++];
+        int peer_port = atoi(argv[offset++]);
+
+        primaryServers.emplace_back(peer_unique_id,  peer_port, peer_ip);
+    }
+
+    if (!socket.Init(port)) {
+        std::cout << "Socket initialization failed" << std::endl;
+        return 0;
+    }
+
+    std::vector<std::thread> thread_vector;
+
+    while ((new_socket = socket.Accept())) {
+
+        std::thread balancer_thread(&LoadBalancerStub::BalancerThread,
+                                    &balancerStub, std::move(new_socket));
+        thread_vector.push_back(std::move(balancer_thread));
+    }
     return 0;
 }
