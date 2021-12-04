a7a3b651aaf5d1920914636c4543427cdd4b3675
osubasi1 <osmansubasi307@gmail.com>
2021-12-03 21:32:19 -0500

    updated to read a specific customer

--- laptop_factory_src/ClientMain.cpp
+++ laptop_factory_src/ClientMain.cpp
@@ -34,16 +34,28 @@ int main(int argc, char *argv[]) {
 
 
 	timer.Start();
-	for (int i = 0; i < num_customers; i++) {
+	if (request_type == 6) {
 		auto client_cls = std::shared_ptr<ClientThreadClass>(new ClientThreadClass());
 		std::thread client_thread(&ClientThreadClass::ThreadBody, client_cls,
-                                  ip, port, i, num_orders, request_type);
+                                  ip, port, num_customers, 1, request_type);
 
 		client_vector.push_back(std::move(client_cls));
 		thread_vector.push_back(std::move(client_thread));
-	}
-	for (auto& th : thread_vector) {
-		th.join();
+		thread_vector[0].join();
+	} else {
+
+	
+		for (int i = 0; i < num_customers; i++) {
+			auto client_cls = std::shared_ptr<ClientThreadClass>(new ClientThreadClass());
+			std::thread client_thread(&ClientThreadClass::ThreadBody, client_cls,
+									ip, port, i, num_orders, request_type);
+
+			client_vector.push_back(std::move(client_cls));
+			thread_vector.push_back(std::move(client_thread));
+		}
+		for (auto& th : thread_vector) {
+			th.join();
+		}
 	}
 	timer.End();
 
